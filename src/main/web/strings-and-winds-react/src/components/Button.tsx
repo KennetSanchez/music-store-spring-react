b9da506ad53e01e9322f97fee730729e6bd9c1c1
zacwastaken <doicirohangnan@gmail.com>
2022-11-24 13:46:45 -0500

    continued login card styling

--- src/main/web/strings-and-winds-react/src/components/Button.tsx
+++ src/main/web/strings-and-winds-react/src/components/Button.tsx
@@ -1,14 +1,35 @@
+import React from "react";
+
 export const Button = (
     props: {
-        isSubmit : string,
+        onClick? : {(e: React.MouseEvent) : void},
+        isSubmit : boolean,
         type : string,
+        rounded : string,
         label : string
     }
 ) => {
     const bType : string = props.isSubmit ? "submit" : "button";
+
+    const transition : string = "transition delay-100 ease-in hover:cursor-pointer";
+    const shadow : string = "drop-shadow-md";
+    const squareSize : string = "h-14 w-14";
+    const size : string = "h-10 w-36";
+
+    const types = () => {
+        switch (props.type) {
+            case "primary":
+            default:
+                return `${shadow} ${size} bg-purple-700 text-white text-base ${transition} hover:bg-purple-400 hover:text-purple-800`;
+            case "square":
+                return `${shadow} ${size} bg-purple-800 text-purple-200 ${transition} hover:bg-purple-400 hover:text-purple-800`;
+            case "square-danger":
+                return `${shadow} ${squareSize} bg-purple-800 text-purple-200 ${transition} hover:bg-red-600 hover:text-red-200`
+            case "danger":
+                return `${shadow} ${squareSize} bg-red-600 text-red-200 text-base ${transition} hover:bg-red-200 hover:text-red-600`;
+        }
+    }
     return (
-        <input type={bType}>
-            {props.label}
-        </input>
+        <input type={bType} onClick={props.onClick} className={`${props.rounded} ${types()}`} value={props.label}/>
     );
 }
--- src/main/web/strings-and-winds-react/src/pages/Landing.tsx
+++ src/main/web/strings-and-winds-react/src/pages/Landing.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import {GlassCard} from "../components/GlassCard";
+import {Button} from "../components/Button";
+import {Link} from "react-router-dom";
 
 export const Landing = (
     props: {}
@@ -9,10 +11,13 @@ export const Landing = (
     return (
         <div className={"w-full h-full relative"}>
             <GlassCard position={"z-10 absolute inset-x-0 mx-auto mt-24"} padding={"py-12 px-16"} size={"h-[32rem] w-96"} color={"bg-purple-100/25"}>
-                <div className={"font-rubik text-purple-50 font-normal text-2xl tracking-[0.25em]"}>LOGIN</div>
+                <div className={"text-purple-50 font-normal text-2xl tracking-[0.25em]"}>LOGIN</div>
                 <div className={"h-full flex flex-col items-center justify-between"}>
                     <div>Fields</div>
-                    <div>Buttons and what not</div>
+                    <div className={"flex flex-col items-center justify-center space-y-2"}>
+                        <Button isSubmit={false} type={"primary"} rounded={"rounded-lg"} label={"Log In"}/>
+                        <div className={"underline underline-offset-4 text-xs text-purple-50 hover:text-purple-300 hover:cursor-pointer"}>Dont have an account yet?</div>
+                    </div>
                 </div>
             </GlassCard>
             <div className={"absolute h-full w-full bg-black/20"}/>
