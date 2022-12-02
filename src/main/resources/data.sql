bd23b363e8b0cc8b19372480692985f081da69a8
Kennet Santiago Sánchez Roldán <1006229463@u.icesi.edu.co>
2022-12-01 20:11:56 -0500

    Database bug fixed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,6 +7,8 @@ INSERT INTO USER_PERMISSION (permission_id, uri, permission_key, permission_meth
 INSERT INTO ROLE_PERMISSION (role_id, permission_id) VALUES
     ('2e72ed53-f5e2-4f7a-bd86-8aadcadeb4eb', 'ccc7ff73-1989-413a-ab52-9bec7a049e33') ON CONFLICT DO NOTHING;
 
+DELETE FROM USERS WHERE id='00000000-0000-0000-0000-000000000000';
+
 INSERT INTO USERS (id, role_id, email, address, phone_number, first_name, last_name, hashed_password) VALUES
     ('00000000-0000-0000-0000-000000000000', '2e72ed53-f5e2-4f7a-bd86-8aadcadeb4eb', 'icesista@icesi.edu.com', 'Calle siempre viva 123 no es falsa','+573009876543', 'Root', 'Admin', 'c1c224b03cd9bc7b6a86d77f5dace40191766c485cd55dc48caf9ac873335d6f');
 
