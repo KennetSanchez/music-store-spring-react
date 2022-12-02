2ade690c10f13fd660d22e2b3fccd3da7d08a0b4
Samuel Hernandez <1006011433@u.icesi.edu.co>
2022-12-01 22:33:34 -0500

    added searchbar component

---
+++ src/main/web/strings-and-winds-react/src/components/SearchBar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const SearchBar = (
+    props: {
+        prompt : string,
+        handleChange : { (e: React.ChangeEvent<HTMLInputElement>) : any },
+    }
+) => {
+    const hoverEffect : string = `transition ease-in-out duration-[400ms] hover:bg-purple-100/30`;
+    const glassEffect : string = `drop-shadow-md backdrop-blur-md bg-purple-100/10`;
+    const style : string = `w-full h-16 px-8 py-4 flex justify-center items-center space-x-8 ${glassEffect} rounded-md ${hoverEffect}`;
+
+    const toggleUnderline = (e: React.FocusEvent ) => {
+        const span = document.getElementById("underlineBar") as HTMLDivElement;
+        const list = span.classList;
+        if (list.contains("bg-purple-900")) list.remove("bg-purple-900");
+        else list.add("bg-purple-900");
+    }
+
+    return (
+        <div className={style}>
+            <div id={"google"} className={"m-0 w-8 h-8 bg-center bg-no-repeat"} style={{backgroundImage: 'url("/icons/search.svg")'}}/>
+            <div className={"flex flex-col w-full h-full justify-between"}>
+                <input onFocus={toggleUnderline} onBlur={toggleUnderline} type="text" placeholder={props.prompt} onChange={props.handleChange} className={`focus:outline-none w-full h-full bg-transparent placeholder:text-purple-900`}/>
+                <div id={"underlineBar"} className={"h-0.5 w-full"}/>
+            </div>
+        </div>
+    );
+}
--- src/main/web/strings-and-winds-react/src/pages/Home.tsx
+++ src/main/web/strings-and-winds-react/src/pages/Home.tsx
@@ -1,18 +1,28 @@
 import React, { useContext } from "react";
 import { UserToken } from "../App";
+import {HeroSection} from "../components/HeroSection";
+import {SearchBar} from "../components/SearchBar";
 
 export const Home = (
     props: {}
 ) => {
     const {token, setToken} = useContext(UserToken);
     document.getElementById("body")?.setAttribute("class", "overflow-auto")
-    
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const query : string = (e.target as HTMLInputElement).value;
+
+    }
     
     return (
-        <div>
-            <div className={"absolute w-full h-full px-24 pt-32"}>
-                
+        <div className={"w-full h-full relative"}>
+            <div className={"absolute w-full h-full px-24 pt-32 flex flex-col space-y-32"}>
+                <SearchBar prompt={"Type to search..."} handleChange={handleSearch}/>
+                <div>
+                    This is where the items would go...
+                </div>
             </div>
+            <HeroSection/>
         </div>
     );
 }
